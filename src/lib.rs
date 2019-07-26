e1ea2fd884cd9cca6c55e62340c875966aade7fa
Linus Keiser <linus@keiser.co>
2019-07-26 13:04:22 +0200

    ft: Refine error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
 //! This crate contains bindings for the RaspberryPi's vcgencmd cli tool
-
 use subprocess::{Exec, PopenError, Redirection};
+use std::num::ParseIntError;
+
+// "vcgencmd" must be in PATH
+const VCGENCMD_INVOCATION: &str = "vcgencmd";
+
+#[derive(Debug)]
+pub enum ExecutionError {
+    Popen(PopenError),
+    Parse(ParseIntError),
+}
 
 pub enum ClockSrc {
     Arm,
@@ -26,9 +35,9 @@ pub enum Cmd {
     MeasureTemp,
 }
 
-/// Execute the given command and capture its std_output
-fn exec_command(command: Cmd, arg: Src) -> Result<String, PopenError> {
-    let vcgencmd_output = Exec::cmd("vcgencmd")
+/// Execute the given command and capture its std_output without modifying it
+pub fn exec_command(command: Cmd, arg: Src) -> Result<String, PopenError> {
+    let vcgencmd_output = Exec::cmd(VCGENCMD_INVOCATION)
         .arg(resolve_command(command))
         .arg(resolve_src(arg))
         .stdout(Redirection::Pipe)
@@ -36,6 +45,13 @@ fn exec_command(command: Cmd, arg: Src) -> Result<String, PopenError> {
         .stdout_str();
 
     Ok(vcgencmd_output)
+}
+
+/// Measure the clock of the selected `ClockSrc`, returning the frequency as isize
+pub fn measure_clock(src: Src) -> Result<isize, ExecutionError> {
+    let output = exec_command(Cmd::MeasureClock, src).map_err(ExecutionError::Popen)?;
+    let frequency = output.split("=").collect::<Vec<_>>()[1].parse::<isize>().map_err(ExecutionError::Parse)?;
+    Ok(frequency)
 }
 
 fn resolve_command(cmd: Cmd) -> String {
@@ -68,11 +84,6 @@ fn resolve_src(src: Src) -> String {
     source
 }
 
-pub fn measure_clock(src: Src) -> Result<String, PopenError> {
-    let output = exec_command(Cmd::MeasureClock, src)?;
-
-    Ok(output)
-}
 
 #[cfg(test)]
 mod tests {
