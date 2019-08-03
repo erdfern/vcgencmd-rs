df1f15643a417ee8c104e1317113b079a5e24d77
Linus Keiser <linus@keiser.co>
2019-08-03 11:05:51 +0200

    fix: Fix integration test. Still useless.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -3,7 +3,7 @@ extern crate vcgencmd;
 #[cfg(target_arch = "arm")]
 #[test]
 fn test_measure_clock() {
-    let output = vcgencmd::measure_clock(vcgencmd::Src::Clock(ClockSrc::Arm)).unwrap();
+    let output = vcgencmd::measure_clock(vcgencmd::Src::Clock(vcgencmd::ClockSrc::Arm)).unwrap();
     dbg!(&output);
     // Idiotic
 }
